10ff8b39efb58dcc1596c1174be15f892df4e121
Dra1noo <D3marpayne@gmail.com>
2024-11-29 07:15:00 -0700

    Add files via upload

---
+++ GameBoy_Website (1)/js/global.js
@@ -0,0 +1,3 @@
+
+document.getElementById('global-header').innerHTML = '<nav><a href="index.html">Home</a> | <a href="about.html">About</a> | <a href="product.html">Product</a> | <a href="contact.html">Contact</a></nav>';
+document.getElementById('global-footer').innerHTML = '<p>&copy; 2024 Game Boy Fansite</p>';
